044299ffaafa603db6532ac2d1b064e392ae89d1
Arxeno <rmfbkw04@gmail.com>
2023-08-21 05:32:04 +0700

    fix: minor styling

--- src/components/sections/daily-checkup/ChooseCalendarSection.tsx
+++ src/components/sections/daily-checkup/ChooseCalendarSection.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function ChooseCalendarSection() {
   return (
-    <div className="px-4">
+    <div className="px-4 flex flex-col gap-8">
       <div className="text-center flex flex-col gap-3">
         <h1 className="font-bold text-2xl">Pilih Tanggal Laporan</h1>
         <h2 className="font-bold text-xl">Sub-group G1</h2>
