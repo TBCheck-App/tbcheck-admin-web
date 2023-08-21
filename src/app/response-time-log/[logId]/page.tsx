f467998e67704f91d8c2ab78119ceba1b023744d
Arxeno <rmfbkw04@gmail.com>
2023-08-21 16:28:36 +0700

    fix: add authorization

--- src/app/response-time-log/[logId]/page.tsx
+++ src/app/response-time-log/[logId]/page.tsx
@@ -1,94 +1,112 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import ButtonBack from "@/components/buttons/ButtonBack";
+import { tokenIsValid } from "@/utils/auth";
+import { useRouter } from "next/navigation";
 
 function ResponseTimeLogDetail() {
-  return (
-    <main className="flex flex-col gap-5">
-      <div className="px-4">
-        <ButtonBack
-          buttonText="Kembali"
-          destPage={``}
-        />
-      </div>
+  const router = useRouter();
+  const [render, setRender] = useState<boolean>(false);
 
-      <div className="mx-5 flex flex-col gap-[38px]">
-        <div className="flex justify-center items-center">
-          <h1 className="font-bold text-2xl">Detail Log</h1>
+  useEffect(() => {
+    tokenIsValid().then((res) => {
+      setRender(res);
+
+      if (res == false) {
+        router.push("/signin");
+      }
+    });
+  }, []);
+
+  if (render) {
+    return (
+      <main className="flex flex-col gap-5">
+        <div className="px-4">
+          <ButtonBack
+            buttonText="Kembali"
+            destPage={`/response-time-log`}
+          />
         </div>
 
-        <div className="flex flex-col gap-7">
-          <div className="flex flex-col gap-2">
-            <h2 className="font-bold">Nama Lengkap</h2>
-            <div className="flex justify-between items-center">
-              <p>Azmi Ramadisha</p>
-              <a
-                href=""
-                className="text-sm font-medium text-[#5497F6] flex flex-row gap-4"
-              >
-                <p>Lihat data peserta</p>
-                <Image
-                  src="/pencil-square.svg"
-                  alt=""
-                  width={20}
-                  height={20}
-                />
-              </a>
+        <div className="mx-5 flex flex-col gap-[38px]">
+          <div className="flex justify-center items-center">
+            <h1 className="font-bold text-2xl">Detail Log</h1>
+          </div>
+
+          <div className="flex flex-col gap-7">
+            <div className="flex flex-col gap-2">
+              <h2 className="font-bold">Nama Lengkap</h2>
+              <div className="flex justify-between items-center">
+                <p>Azmi Ramadisha</p>
+                <a
+                  href=""
+                  className="text-sm font-medium text-[#5497F6] flex flex-row gap-4"
+                >
+                  <p>Lihat data peserta</p>
+                  <Image
+                    src="/pencil-square.svg"
+                    alt=""
+                    width={20}
+                    height={20}
+                  />
+                </a>
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <h2 className="font-bold">Group</h2>
+              <p>A</p>
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <h2 className="font-bold">Sub-Group</h2>
+              <p>A1</p>
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <h2 className="font-bold">Hari, Tanggal Respon</h2>
+              <p>Rabu, 26/07/2023</p>
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <h2 className="font-bold">Waktu Respon</h2>
+              <p>19:30:36 WIB</p>
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <h2 className="font-bold">Jeda Respon dari Notifikasi</h2>
+              <p>10 Menit</p>
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <h2 className="font-bold">Variasi Desain Notifikasi</h2>
+              <p>Variasi 0</p>
             </div>
           </div>
 
-          <div className="flex flex-col gap-2">
-            <h2 className="font-bold">Group</h2>
-            <p>A</p>
-          </div>
+          <div className="flex flex-col gap-5">
+            <p className="font-bold text-[#546881]">Hasil Jawaban Pertanyaan</p>
 
-          <div className="flex flex-col gap-2">
-            <h2 className="font-bold">Sub-Group</h2>
-            <p>A1</p>
-          </div>
+            <div className="flex flex-col gap-2">
+              <h2 className="font-bold">
+                Menurut Anda, seberapa penting Anda harus menggunakan masker?
+              </h2>
+              <p>Sangat tidak peduli</p>
+            </div>
 
-          <div className="flex flex-col gap-2">
-            <h2 className="font-bold">Hari, Tanggal Respon</h2>
-            <p>Rabu, 26/07/2023</p>
-          </div>
-
-          <div className="flex flex-col gap-2">
-            <h2 className="font-bold">Waktu Respon</h2>
-            <p>19:30:36 WIB</p>
-          </div>
-
-          <div className="flex flex-col gap-2">
-            <h2 className="font-bold">Jeda Respon dari Notifikasi</h2>
-            <p>10 Menit</p>
-          </div>
-
-          <div className="flex flex-col gap-2">
-            <h2 className="font-bold">Variasi Desain Notifikasi</h2>
-            <p>Variasi 0</p>
+            <div className="flex flex-col gap-2">
+              <h2 className="font-bold">
+                Menurut Anda, seberapa besar kepedulian Anda untuk menggunakan
+                masker?
+              </h2>
+              <p>Sangat tidak peduli</p>
+            </div>
           </div>
         </div>
-
-        <div className="flex flex-col gap-5">
-          <p className="font-bold text-[#546881]">Hasil Jawaban Pertanyaan</p>
-
-          <div className="flex flex-col gap-2">
-            <h2 className="font-bold">
-              Menurut Anda, seberapa penting Anda harus menggunakan masker?
-            </h2>
-            <p>Sangat tidak peduli</p>
-          </div>
-
-          <div className="flex flex-col gap-2">
-            <h2 className="font-bold">
-              Menurut Anda, seberapa besar kepedulian Anda untuk menggunakan
-              masker?
-            </h2>
-            <p>Sangat tidak peduli</p>
-          </div>
-        </div>
-      </div>
-    </main>
-  );
+      </main>
+    );
+  }
 }
 
 export default ResponseTimeLogDetail;
