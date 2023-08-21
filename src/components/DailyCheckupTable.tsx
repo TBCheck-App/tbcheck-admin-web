962d7b38802ac8a41f609c2246f56e907b4991b9
Arxeno <rmfbkw04@gmail.com>
2023-08-21 08:39:28 +0700

    remove unnecessary comments

--- src/components/DailyCheckupTable.tsx
+++ src/components/DailyCheckupTable.tsx
@@ -42,15 +42,6 @@ function DailyCheckupTable({ risk }: Props) {
             Berisiko Rendah
           </p>
         ) : null}
-        {/* <p className="py-1 w-[90%] bg-[#FCC0CF] text-[#6F182D] font-semibold text-center rounded-[100px]">
-          Berisiko Tinggi
-        </p> */}
-        {/* <p className="py-1 w-[90%] bg-[#FFF2B0] text-[#736000] font-semibold text-center rounded-[100px]">
-          Berisiko
-        </p> */}
-        {/* <p className="py-1 w-[90%] bg-[#CDEFEC] text-[#2B5B58] font-semibold text-center rounded-[100px]">
-          Berisiko Rendah
-        </p> */}
       </div>
     </div>
   );
