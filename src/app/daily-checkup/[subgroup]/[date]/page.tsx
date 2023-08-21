99621bd81833efc9d4745d8f2ff7cdd1391fc257
Arxeno <rmfbkw04@gmail.com>
2023-08-21 08:19:22 +0700

    feat: finish UI daily table

--- src/app/daily-checkup/[subgroup]/[date]/page.tsx
+++ src/app/daily-checkup/[subgroup]/[date]/page.tsx
@@ -1,4 +1,6 @@
+import DailyCheckupTable from "@/components/DailyCheckupTable";
 import ButtonBack from "@/components/buttons/ButtonBack";
+import ButtonOutlined from "@/components/buttons/ButtonOutlined";
 import Image from "next/image";
 import React from "react";
 
@@ -16,14 +18,14 @@ function DailyCheckUpData({ params }: Props) {
         />
       </div>
 
-      <div>
+      <div className="flex flex-col gap-6 mx-5">
         <div className="flex flex-col items-center gap-3">
           <h1 className="font-bold text-2xl">History</h1>
           <h2 className="font-bold text-xl">Sub-group G1</h2>
           <p>Kamis, 24 Juli 2023</p>
         </div>
 
-        <div className="bg-[#FCFDEE] mx-5 p-3 flex flex-col gap-2">
+        <div className="bg-[#FCFDEE] p-3 flex flex-col gap-2">
           <h3 className="text-[#1C998B] font-bold">Ringkasan</h3>
 
           <div className="text-sm flex flex-col gap-2">
@@ -49,6 +51,63 @@ function DailyCheckUpData({ params }: Props) {
               height={48}
             />
           </div>
+        </div>
+
+        <ButtonOutlined
+          icons="/download.svg"
+          text="Unduh Riwayat"
+        />
+
+        <div className="border rounded">
+          <header className="flex flex-row p-4 gap-4">
+            <div className="rounded-[32px] flex flex-row gap-1 border py-3 px-2 w-[286px]">
+              <Image
+                src="/magnifying-glass.svg"
+                alt=""
+                width={16}
+                height={16}
+              />
+              <input
+                type="text"
+                className="focus:outline-none text-xs"
+                placeholder="Search"
+              />
+            </div>
+
+            <button>
+              <Image
+                src="/funnel.svg"
+                alt=""
+                width={16}
+                height={16}
+              />
+            </button>
+          </header>
+
+          <div
+            id="daily-checkup-columns"
+            className="text-xs font-semibold grid text-center h-14 border-t grid-daily-checkup-table"
+          >
+            <div className="flex flex-row justify-center items-center p-2">
+              <Image
+                src="/bars-arrow-down.svg"
+                alt=""
+                width={16}
+                height={16}
+              />
+              <p>Waktu Check Up</p>
+            </div>
+            <div className="flex justify-center items-center p-2">
+              <p>Nama Peserta</p>
+            </div>
+            <div className="flex justify-center items-center p-2">
+              <p>Hasil Check Up</p>
+            </div>
+          </div>
+
+          <DailyCheckupTable risk="HIGH_RISK" />
+          <DailyCheckupTable risk="MEDIUM_RISK" />
+          <DailyCheckupTable risk="LOW_RISK" />
         </div>
       </div>
     </main>
---
+++ src/components/DailyCheckupTable.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+
+interface Props {
+  // dailyReport: {
+  //   id: string;
+  //   reportedAt: string;
+  //   result: "HIGH_RISK" | "MEDIUM_RISK" | "LOW_RISK";
+  //   name: string;
+  // };
+  risk: "HIGH_RISK" | "MEDIUM_RISK" | "LOW_RISK";
+}
+
+function DailyCheckupTable({ risk }: Props) {
+  return (
+    <div className="text-xs h-12 grid grid-daily-checkup-table">
+      <div className="p-2 flex justify-center items-center">
+        <p>14:00:00</p>
+      </div>
+      <div className="p-2 flex justify-center items-center">
+        <a
+          className="font-semibold text-[#5497F6]"
+          href=""
+        >
+          Azmi Ramadisha
+        </a>
+      </div>
+      <div className="p-2 flex justify-center items-center">
+        {risk == "HIGH_RISK" ? (
+          <p className="py-1 w-[90%] bg-[#FCC0CF] text-[#6F182D] font-semibold text-center rounded-[100px]">
+            Berisiko Tinggi
+          </p>
+        ) : null}
+
+        {risk == "MEDIUM_RISK" ? (
+          <p className="py-1 w-[90%] bg-[#FFF2B0] text-[#736000] font-semibold text-center rounded-[100px]">
+            Berisiko
+          </p>
+        ) : null}
+
+        {risk == "LOW_RISK" ? (
+          <p className="py-1 w-[90%] bg-[#CDEFEC] text-[#2B5B58] font-semibold text-center rounded-[100px]">
+            Berisiko Rendah
+          </p>
+        ) : null}
+        {/* <p className="py-1 w-[90%] bg-[#FCC0CF] text-[#6F182D] font-semibold text-center rounded-[100px]">
+          Berisiko Tinggi
+        </p> */}
+        {/* <p className="py-1 w-[90%] bg-[#FFF2B0] text-[#736000] font-semibold text-center rounded-[100px]">
+          Berisiko
+        </p> */}
+        {/* <p className="py-1 w-[90%] bg-[#CDEFEC] text-[#2B5B58] font-semibold text-center rounded-[100px]">
+          Berisiko Rendah
+        </p> */}
+      </div>
+    </div>
+  );
+}
+
+export default DailyCheckupTable;
