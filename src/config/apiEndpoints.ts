94ecbeab393178a3273367722623b4a9440dab0e
Arxeno <rmfbkw04@gmail.com>
2023-08-22 15:20:44 +0700

    add daily checkup endpoint

--- src/config/apiEndpoints.ts
+++ src/config/apiEndpoints.ts
@@ -2,6 +2,7 @@ const apiEndpoints = {
   login: "/auth/login",
   getAllUser: "/user/all",
   getUserDetail: (id: string) => `/user/${id}`,
+  getAllDailyCheckup: "/daily-checkup",
 };
 
 export default apiEndpoints;
